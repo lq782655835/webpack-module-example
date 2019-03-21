2039c7c1e5f1a8cc1cd31b938611cc51851a7456
springleo <782655835@qq.com>
2019-03-21 18:03:43 +0800

    webpack module example

---
+++ src/add.js
@@ -0,0 +1,4 @@
+export default function(a, b) {
+    let { name } = { name: 'hello world,'}
+    return name + a + b
+}
---
+++ src/main.js
@@ -0,0 +1,3 @@
+// import Add from './add'
+let Add = require('./add').default
+console.log(Add, Add(1, 2), 123)
